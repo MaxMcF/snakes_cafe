7b8443b0a5657c9ed0a9f8e39e76eb7b43ea3f2a
MAX MCFARLAND <mmcfarland522@gmail.com>
2018-08-17 13:52:02 -0700

    added some tests

--- test_snakes_cafe.py
+++ test_snakes_cafe.py
@@ -5,10 +5,13 @@ def test_order_function_exists():
     assert sc.order_something
 
 
-
-
 def test_csv_decoder():
     expected = {'animal':{'cat':[8.6, 4], 'blue whale':[300000, 2]}}
     actual = sc.menu_decoder('cat,animal,8.6,4\nblue whale,animal,300000,2')
     assert expected == actual
 
+
+def test_format_num():
+    expected = '$13.87'
+    actual = sc.format_nums(13.87454321)
+    assert expected == actual
